a085ade7186f75c0cb7d9f87b151e669ee5a8c1d
Jason Walworth <jrwalworth@gmail.com>
2022-04-19 18:22:54 -0700

    file setup.

---
+++ src/components/Form.js
@@ -0,0 +1,24 @@
+import React, {useReducer} from 'react';
+import './Form.css';
+
+
+const initialState = {
+    firstName : {
+        value: '',
+        error: null,
+    },
+    lastName : {
+        value: '',
+        error: null,
+    },
+    email : {
+        value: '',
+        error: null,
+    }
+};
+
+
+
+const Form = (props) => {
+
+}
